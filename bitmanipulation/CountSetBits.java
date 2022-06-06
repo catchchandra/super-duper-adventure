641cc7d51e14ff99fda3657c4e09f2960f7c0ab6
catchchandra <98239965+catchchandra@users.noreply.github.com>
2022-06-05 17:32:04 -0700

    Create CountSetBits.java

---
+++ bitmanipulation/CountSetBits.java
@@ -0,0 +1,19 @@
+//Brian Kernighan's Algorithm
+
+//The main idea behind this algorithm is that when we subtract one from any number, it inverts all the bits after the rightmost set bit
+
+class CountSetBit {
+    private static int helper(int n) {
+        int count = 0;
+        while (n > 0) {
+            n &= (n - 1);
+            count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int number = 125;
+        System.out.println("SetBit Count is : " + helper(number));
+    }
+}
